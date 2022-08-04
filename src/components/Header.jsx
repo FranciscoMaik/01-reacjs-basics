6b610bb8cadf249711e7cdcca30022deefc19da6
FranciscoMaik <chiconunes3081@gmail.com>
2022-08-04 20:35:35 -0300

    feat: personalization header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css"
 
+import igniteLogo from "../assets/ignite-logo.svg";
+
 export function Header(params) {
     return (
         <header className={styles.header}>
-            <strong>Ignite Feed</strong>
+            <img src={igniteLogo} alt="logo do ignite" />
         </header>
     );
 }
