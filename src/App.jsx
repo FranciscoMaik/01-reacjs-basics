08ee73ef017afa2a1915480e8e3413ab0b91b12d
FranciscoMaik <chiconunes3081@gmail.com>
2022-08-04 01:24:14 -0300

    feat: create styles default

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { Post } from "./Post" 
+import { Header } from "./components/Header"
+
+import "./global.css"
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Francisco Maik" 
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae provident laborum quis reprehenderit. Quidem, quos ad! Alias a, unde eius sequi voluptas, eos, amet veniam magnam delectus enim iusto necessitatibus?"
