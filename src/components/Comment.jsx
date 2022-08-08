b8db43d3cc5f3fd6adcc07f01e1c8ce5993bb4fc
FranciscoMaik <chiconunes3081@gmail.com>
2022-08-08 15:30:18 -0300

    feat: create component avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,24 +1,26 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
 import styles from "./Comment.module.css";
 
 export function Comment() {
 	return (
 		<div className={styles.comment}>
-			<img src="https://github.com/FranciscoMaik.png" />
+			<Avatar hasBorder={false} src="https://github.com/FranciscoMaik.png" />
 
 			<div className={styles.commentBox}>
 				<div className={styles.commentContent}>
 					<header>
 						<div className={styles.authorAndTime}>
 							<strong>Francisco Maik</strong>
-							<time 
-								title="11 de Outubro às 08:12h"  
+							<time
+								title="11 de Outubro às 08:12h"
 								dateTime="2022-10-11 08:12:12">Cerca de 1h atrás
 							</time>
 						</div>
 
 						<button title="Deletar comentário">
-							<Trash size={24}/>
+							<Trash size={24} />
 						</button>
 					</header>
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { PencilLine } from "phosphor-react"
 
+import { Avatar } from "./Avatar"
+
 import styles from "./Sidebar.module.css"
 
 export function Sidebar(){
@@ -11,10 +13,7 @@ export function Sidebar(){
             />
 
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar}
-                    src="https://avatars.githubusercontent.com/u/20601076?v=4"
-                />
+                <Avatar src="https://github.com/FranciscoMaik.png"/>
 
                 <strong>Francisco Maik</strong>
                 <span>Tech Líder</span>
