26a63a28d030178ce9fb813d3ccadce76ea1ebc7
FranciscoMaik <chiconunes3081@gmail.com>
2022-08-26 12:57:34 -0300

    feat: delete comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,11 @@ import { Avatar } from "./Avatar";
 
 import styles from "./Comment.module.css";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+	function handleDeleteComment() {
+		onDeleteComment(content);
+	}
+
 	return (
 		<div className={styles.comment}>
 			<Avatar hasBorder={false} src="https://github.com/FranciscoMaik.png" />
@@ -19,7 +23,7 @@ export function Comment({ content }) {
 							</time>
 						</div>
 
-						<button title="Deletar comentário">
+						<button title="Deletar comentário" onClick={handleDeleteComment}>
 							<Trash size={24} />
 						</button>
 					</header>
